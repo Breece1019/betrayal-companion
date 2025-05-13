cfd86e591bfada7991dfb2943841705368e69614
Breece1019 <breece1019@gmail.com>
2025-05-13 00:04:13 -0400

    added ProfessorLongfellow stats; TODO add some controller methods to manipulate stats

--- BetrayalAPI/Controllers/PlayerController.cs
+++ BetrayalAPI/Controllers/PlayerController.cs
@@ -1,22 +1,32 @@
+using BetrayalAPI.Models.Players;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BetrayalAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/betrayal/[controller]")]
     public class PlayerController : ControllerBase
     {
-        // GET api/custom
+        [HttpGet("traits/{trait}")]
+        public IActionResult GetTraitValue(int trait)
+        {
+            return Ok(new { Message = $"TODO: You want trait number: {trait}" });
+        }
+
+        // GET api/player
         [HttpGet]
         public IActionResult Get()
         {
             return Ok(new { Message = "Hello, world!" });
         }
 
-        // GET api/custom/5
+        // GET api/player/5
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
+            IPlayer player = new ProfessorLongfellow();
+            player.Might.Pointer = 3;
+
             return Ok(new { Message = $"You requested item {id}" });
         }
     }
--- BetrayalAPI/Models/Players/IPlayer.cs
+++ BetrayalAPI/Models/Players/IPlayer.cs
@@ -1,10 +1,18 @@
 
-namespace BetrayalAPI.Models;
+namespace BetrayalAPI.Models.Players;
 
 public interface IPlayer {
     public string Name { get; set; }
-    public int[] Speed { get; set; }
-    public int[] Might { get; set; }
-    public int[] Sanity { get; set; }
-    public int[] Knowledge { get; set; }
+    public Trait Speed { get; set; }
+    public Trait Might { get; set; }
+    public Trait Sanity { get; set; }
+    public Trait Knowledge { get; set; }
+}
+
+public class Trait
+{
+    public int[] Values { get; set; }
+    public int Pointer { get; set; }
+
+    public int CurrentValue => Values[Pointer];
 }
