d45b249ca48638ce4a4832ec3763b50203bb2286
Breece1019 <breece1019@gmail.com>
2025-05-15 22:06:42 -0400

    remove test controllers

--- BetrayalAPI/Controllers/PlayerController.cs
+++ BetrayalAPI/Controllers/PlayerController.cs
@@ -1,8 +1,6 @@
-using BetrayalAPI.Constants;
 using BetrayalAPI.Helpers;
 using BetrayalAPI.Models.Players;
 using BetrayalAPI.Models.Request;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BetrayalAPI.Controllers
@@ -21,23 +19,6 @@ namespace BetrayalAPI.Controllers
             return result != -1
                     ? Ok($"You want trait: {traitRequest.Trait} from player: {traitRequest.PlayerName}, which is: {result}")
                     : NotFound($"Unable to Locate trait: {traitRequest.Trait} for player: {traitRequest.PlayerName}");
-        }
-
-        // GET api/player
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok(new { Message = "Hello, world!" });
-        }
-
-        // GET api/player/5
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
-        {
-            IPlayer player = new ProfessorLongfellow();
-            player.Might.Pointer = 3;
-
-            return Ok(new { Message = $"You requested item {id}" });
         }
     }
 }
