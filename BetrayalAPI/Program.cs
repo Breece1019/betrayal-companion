7a0f34a67e0b32b81c2d3d22f984645a1048b4f5
Breece1019 <breece1019@gmail.com>
2025-05-19 20:51:32 -0400

    bad cors policy

--- BetrayalAPI/Program.cs
+++ BetrayalAPI/Program.cs
@@ -1,8 +1,11 @@
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://0.0.0.0:9101");
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -10,7 +13,20 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()     // Allow all origins (or specify a specific origin)
+              .AllowAnyMethod()     // Allow all HTTP methods (GET, POST, etc.)
+              .AllowAnyHeader();    // Allow all headers
+    });
+});
+
 var app = builder.Build();
+
+// Use CORS
+app.UseCors("AllowAllOrigins");
 
 app.MapControllers();
 
